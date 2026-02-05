13f5797df4fe986ee3aacd91e61cee8b61627c2e
Jose <106478280+jlca0@users.noreply.github.com>
2026-02-05 13:01:30 +0100

    folder reallocation

---
+++ counter_machine/IDE.py
@@ -0,0 +1,141 @@
+import tkinter as tk
+from tkinter import filedialog, scrolledtext
+
+from minsky import Minsky
+
+
+class Editor:
+    def __init__(
+        self, 
+         N:int
+    ) -> None:
+        """
+        Constructor de un objeto de la clase Editor que crea una ventana con un editor de programas
+        para máquinas de registros con N registros.
+
+        Atributos:
+            ventana: (tk.Tk) Una ventana de la librería tkinter.
+            N: (int) Número de registros que usa la máquina de registros con la que se va a trabajar.
+            archivo: (str) Dirección del archivo al que tiene acceso el editor.
+            texto: (tk.Text) Ventana de texto (editable).
+            consola: (tk.Text) Ventana de texto (no editable).
+
+        Argumentos:
+            N (int): Número de registros que se van a usar.
+        """
+        self.ventana = tk.Tk()
+        self.ventana.title("Editor Minsky")  # Creamos una ventana con nombre "Editor Minsky".
+ 
+        self.N = N
+        self.archivo = None  # La ruta de acceso del archivo de texto que vamos a editar.
+        self._crear_interfaz()  # Método prvado que crea la interfaz.
+        self.ventana.mainloop()  # Mostramos la pantalla.
+    
+    def _crear_interfaz(self)->None:
+        """
+        Método privado que configura los elementos que aparecen en la ventana "Editor Minsky".
+        """        
+        # Creamos los botones para navegar en el editor. 
+        botones = tk.Frame(self.ventana)  # Agrupamos los botones en un solo frame.
+
+        abrir = tk.Button(botones, text="Abrir", command=self._abrir)
+        guardar = tk.Button(botones, text="Guardar", command=self._guardar)
+        ejecutar = tk.Button(botones, text="Ejecutar", command=self._ejecutar)
+        depurar = tk.Button(botones, text="Depurar", command=self._depurar)
+
+        abrir.grid(row=0,column=0)  # Colocamos el botón en la posición (0,0) de botones.
+        guardar.grid(row=0,column=1)
+        ejecutar.grid(row=0,column=2)
+        depurar.grid(row=0,column=3)
+
+        botones.grid(row=0,column=0)  # Colocamos el frame botones dentro de ventana.
+
+        # Creamos una ventana para el texto.
+        self.texto = scrolledtext.ScrolledText(self.ventana, font=("Courier", 10), bg='grey', fg='blue')
+        self.texto.grid(row=1, column=0)
+        
+        # Creamos una ventana para la consola.
+        self.consola = scrolledtext.ScrolledText(self.ventana, font=("Courier", 9), state='disabled', bg='black', fg='white')
+        self.consola.grid(row=2, column=0)
+        
+    
+    def _abrir(self)->None:
+        """
+        Dada una ruta de acceso (que se guarda como atributo de Minsky) a un fichero .txt que se lee y se vuelca su
+        contenido en la ventana de texto. Lo notificamos en la consola.
+        """
+        ruta = filedialog.askopenfilename(filetypes=[("Texto", "*.txt")])  # Seleccionamos una ruta de acceso.
+        if ruta:
+            fichero = open(ruta, 'r').read()
+            self.texto.delete(1.0, tk.END)  # Borramos el texto que mostraba el editor.
+            self.texto.insert(1.0, fichero) # Volcamos el nuevo archivo.
+            self.archivo = ruta  
+            self._escribir_consola(f"Archivo cargado: {ruta}")
+    
+    def _guardar(self)->None:
+        """
+        Dada una ruta de acceso guarda el texto en pantalla en el fichero de texto abierto o en uno nuevo
+        si es el caso.
+        """
+        if not self.archivo:  # No se ha abierto ningún fichero.
+            self.archivo = filedialog.asksaveasfilename(filetypes=[("Texto", "*.txt")])  # Seleccionamos una dirección de guardado.
+        else:
+            open(self.archivo, 'w').write(self.texto.get(1.0, tk.END))  # Se sobreescribe el fichero.
+        self._escribir_consola(f"Guardado: {self.archivo} \n")
+    
+    def _ejecutar(self)->None:        
+        """
+        Seleccionado un archivo de texto en el editor se ejecuta el programa usando un objeto de clase
+        Minsky, imprime en consola el resultado.
+        """
+        
+        # Ejecutamos el programa seleccionado con esos valores usando un objeto de clase Minsky.
+        maquina = Minsky(self.N)
+        registros_usados = maquina.ejecutar(self.texto.get("1.0", tk.END))
+
+        # Pintamos el proceso en pantalla.
+        self._borrar_consola
+        self._escribir_consola(f"Ejecutando: {self.archivo}...")
+
+        if (registros_usados > 0):  # Minsky.ejecutar devolvía -1 si el programa no terminaba.
+            self._escribir_consola(f"Estado final: {maquina.Registro[:registros_usados]} \n")
+        else:
+            self._escribir_consola(f"El programa no termina, número máximo de iteraciones alcanzado {maquina.iter_max}")
+
+    def _depurar(self)->None:
+        """
+        Seleccionado un archivo de texto en el editor se ejecuta el programa paso a paso 
+        usando un objeto de clase Minsky.
+        """
+        maquina = Minsky(self.N)
+        mensaje = maquina.depurar(self.texto.get("1.0",tk.END))
+
+        self._borrar_consola
+        for linea in reversed(mensaje):
+            self._escribir_consola(linea)
+        self._escribir_consola(f"Depurando: {self.archivo}... \n")  #No distinguimos porque Minsky.depurar siempre devuelve todo, para verificar mal funcionamiento.
+        
+    def _escribir_consola(
+            self, 
+            mensaje:str
+        )->None:
+        """
+        Dado un mensaje (interno) lo pinta en la consola.
+
+        Atributos:
+            mensaje (string): El mensaje que queremos mostrar.
+        """
+        self.consola.config(state='normal')
+        self.consola.insert(1.0,mensaje)
+        self.consola.config(state='disabled')
+
+    def _borrar_consola(self)->None:
+        """
+        Borra el contenido de la consola.
+        """
+        self.consola.config(state='normal')
+        self.consola.delete(1.0, tk.END)
+        self.consola.config(state='disabled')
+
+if __name__ == "__main__":
+    editor = Editor(100)
---
+++ counter_machine/minsky.py
@@ -0,0 +1,112 @@
+import numpy as np
+
+
+class Minsky:
+    def __init__(
+            self, 
+            N:int
+        )->None:
+        """
+        Construye un objeto de la clase Minsky que simula una máquina de registros.
+
+        Atributos:
+            Registro: (array[int]) Valor de cada registro de la máquina de Minsky.
+            iter_max: (int) Número máximo de iteraciones.
+
+        Argumentos:
+            N (int): Número de registros que se van a usar.
+        """
+        self.iter_max = int(1e8)  # Número máximo de iteraciones, gestionamos bucles infinitos.
+        self.Registro = np.zeros(N,dtype=int)  # Inicializamos los registros R1,...,RN con valor 0.
+
+    def ejecutar(
+            self, 
+            archivo:str
+        )->int:
+        """
+        Ejecuta un programa dado usando la máquina de registros.
+
+        Argumentos:
+            archivo: (str) Contenido de un archivo de texto formateado como programa de máquina de resgistros.
+
+        Salida:
+            registros_usados: (int) Número de registros que se usan en el código ejecutado o -1 que codifica un mensaje de error.
+        """
+        programa = self._leer(archivo)
+        registros_usados = len(programa[0])
+        self.Registro[:registros_usados] = np.array(list(programa[0]))
+        estado_actual = 1
+        contador = 0
+        while (estado_actual != 0 and contador < self.iter_max):
+            instruccion = programa[estado_actual]
+            if (len(instruccion) == 3):  # Instrucción del tipo (i,+,j)
+                self.Registro[instruccion[0]-1] += 1
+                estado_actual = instruccion[2]
+            else:  #Instrucción del tipo (i,-,j,k)
+                if (self.Registro[instruccion[0]-1] > 0):
+                    self.Registro[instruccion[0]-1] -= 1
+                    estado_actual = instruccion[2]
+                else:
+                    estado_actual = instruccion[3]
+            contador += 1
+        if (contador == self.iter_max):  # Verificamos por qué acabó el bucle.
+            return -1
+        else:
+            return registros_usados
+
+    def depurar(
+            self, 
+            archivo:str
+        )->list[str]:
+        """
+        Ejecuta un programa dado usando la máquina de registros y almacena en una lista 
+        el estado de la máquina y los registros en cada iteración.
+
+        Argumentos:
+            archivo: (str) Contenido de un archivo de texto formateado como programa de máquina de resgistros.
+
+        Salida:
+            mensaje: (list[str]) Lista de mensajes de depurado mostrados en consola.
+        """
+        programa = self._leer(archivo)
+        registros_usados = len(programa[0])
+        self.Registro[:registros_usados] = np.array(list(programa[0]))
+        estado_actual = 1
+        contador = 0
+        mensaje = [f"Iteración  Estado  Registros \n",f"{contador}          S{estado_actual}          {self.Registro[:registros_usados]} \n"]
+        while (estado_actual != 0 and contador < self.iter_max):
+            contador += 1
+            instruccion = programa[estado_actual]
+            if (len(instruccion) == 3):  # Instrucción del tipo (i,+,j)
+                self.Registro[instruccion[0]-1] += 1
+                estado_actual = instruccion[2]
+            else:  #Instrucción del tipo (i,-,j,k)
+                if (self.Registro[instruccion[0]-1] > 0):
+                    self.Registro[instruccion[0]-1] -= 1
+                    estado_actual = instruccion[2]
+                else:
+                    estado_actual = instruccion[3]
+            mensaje.append(f"{contador}          S{estado_actual}          {self.Registro[:registros_usados]} \n")
+        return mensaje
+
+    def _leer(
+            self, 
+            archivo:str
+        )->list[tuple[int]]:
+        """
+        Lee un archivo .txt conteniendo un programa y lo pasa a un string que comprende _ejecutar.
+
+        Argumentos:
+            archivo: (str) Contenido de un archivo de texto formateado como programa de máquina de resgistros.
+
+        Salida: 
+            programa: (list[tuple[int]]) Lista cuyos elementos son instrucciones para una máquina de registros.
+        """
+        lineas = archivo.split('\n')
+        programa = []
+        for linea in lineas:
+            linea_limpia = linea.strip()          
+            if linea_limpia:
+                instruccion_lista = linea_limpia.split(',')
+                programa.append(tuple(int(x) for x in instruccion_lista))
+        return programa
